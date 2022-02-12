adbf8f006e44efc6f20c9301f0b80d11b7f1a7d6
aito <BoxPistols@users.noreply.github.com>
2022-02-12 22:00:44 +0900

    Debug KHKK on M1

--- app.js
+++ app.js
@@ -1,68 +1,128 @@
-// jQuery
-// $("button:not(.remove)").on("click", function () {
-//   $(".example").css("justify-self", $(this).text());
-// });
+/**
+  // jQuery
+  $("button:not(.remove)").on("click", function () {
+    $(".example").css("justify-self", $(this).text());
+  }); 
+*/
 
-// TODO: 汎用関数化　Class化？ new インスタンス
-// to Vanilla
-var classNameButton = document.querySelectorAll(
-  '.buttons.justifySelf > button'
+/**
+ * TODO: 汎用関数化　Class化？ new インスタンス
+ * TODO: 最終CSSのView
+ *
+ * React化：
+ *  useStateでボタンのテキストを取得、index.innerText?
+ *  実際のクラスターゲットに値を当てる
+ *  style.xxxが鬼門
+ *
+ * DONE: jQuery to Vanilla
+ */
+
+const getSelectorAll = (x) => {
+  return document.querySelectorAll(`.buttons.${x} > button`);
+};
+
+resultJustifySelf = getSelectorAll('justifySelf');
+
+/** 
+console.log(
+`style.justifySelf:${style.justifySelf} \n tx:${tx} \n compStyles[i]:${compStyles.justifySelf}`
 );
+*/
 
-for (let i = 0; i < classNameButton.length; i++) {
-  classNameButton[i].addEventListener('click', function () {
-    let tx = this.innerText;
-    console.log(tx);
-    //     justifySelf
-    let para = document.querySelector('.example');
-    let compStyles = window.getComputedStyle(para);
-    var style = para.style;
-    style.justifySelf = tx;
-    console.log(compStyles.getPropertyValue('justifySelf'));
-  });
-}
+// justify-self
+const getCssProps = (_searchElm, _target, _styling) => {
+  for (let i = 0; i < resultJustifySelf.length; i++) {
+    resultJustifySelf[i].addEventListener('click', function () {
+      let tx = this.innerText;
+      let para = document.querySelector(_target);
 
-// Align
-var ButtonAlignItems = document.querySelectorAll(
-  '.buttons.alignItems > button'
-);
+      let style = para.style[`${_styling}`];
+      // style.justifySelf = tx;
+      style = tx;
 
-for (let i = 0; i < ButtonAlignItems.length; i++) {
-  ButtonAlignItems[i].addEventListener('click', function () {
-    let tx = this.innerText;
-    console.log(tx);
-    //     justifySelf
-    let para = document.querySelector('.container');
-    let compStyles = window.getComputedStyle(para);
-    var style = para.style;
-    style.alignItems = tx;
-    console.log(compStyles.getPropertyValue('alignItems'));
-  });
-}
+      let compStyles = window.getComputedStyle(para);
+      let r = compStyles[`${_styling}`];
+      r = tx
+
+
+      // Test 
+      console.log(style, r);
+    });
+  }
+};
+
+
+
+const resProp = (_searchElm, _target, _styling) => {
+  _res = getSelectorAll(_searchElm);
+  getCssProps(_res, _target, _styling);
+};
+
+resProp('justifySelf', '.example', "justifySelf");
+
+// test
+
+const elem = document.querySelector('.example');
+elem.style.setProperty('color', '#ff0', '');
+elem.style.setProperty('margin-bottom', '30px', 'important');
+
+console.log("x" + elem); // id="div1" の要素の align の値を表示します。
+
+// ----------------
+
+
+
+
+
+
+
+
+
+
+
+// resProp('alignItems', '.container');
+// resProp('justifyContent', '.container');
 
 // justify-content
-var ButtonJustifyContent = document.querySelectorAll(
+const ButtonJustifyContent = document.querySelectorAll(
   '.buttons.justifyContent > button'
 );
 
 for (let i = 0; i < ButtonJustifyContent.length; i++) {
   ButtonJustifyContent[i].addEventListener('click', function () {
     let tx = this.innerText;
-    console.log(tx);
+    // console.log(tx);
     // justifyContent
     let para = document.querySelector('.container');
-    let compStyles = window.getComputedStyle(para);
-    var style = para.style;
+    const style = para.style;
     style.justifyContent = tx;
-    console.log(compStyles.getPropertyValue('justifyContent'));
+    // console.log(compStyles.getPropertyValue('justifyContent'));
   });
 }
 
-// var className = document.querySelectorAll("element");
+// Align
+const ButtonAlignItems = document.querySelectorAll(
+  '.buttons.alignItems > button'
+);
+
+for (let i = 0; i < ButtonAlignItems.length; i++) {
+  ButtonAlignItems[i].addEventListener('click', function () {
+    let tx = this.innerText;
+    // console.log(tx);
+    //     justifySelf
+    let para = document.querySelector('.container');
+    const style = para.style;
+    style.alignItems = tx;
+    // console.log(compStyles.getPropertyValue('alignItems'));
+  });
+}
+
+
+// const className = document.querySelectorAll('element');
 
 // for (let i = 0; i < className.length; i++) {
-//   className[i].addEventListener("click", function () {
-//     this.classList.toggle("example");
+//   className[i].addEventListener('click', function () {
+//     this.classList.toggle('example');
 //     alert(this.classList);
 //   });
 // }
